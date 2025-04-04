5e2fa43a282925a3799696231776f306a8bd24f0
LukasDekan <Lukas.dekan24@student.cart.org>
2025-04-04 14:29:25 -0700

    (Lukas) Initializing game files
    -setting up first few id's for javascript and css

---
+++ main.js
@@ -0,0 +1,7 @@
+    const story = {
+         startChoices: ["accept", "decline", "think about it"],
+         combat: [],
+         actions: [act, eat, talk, combat],
+         caveActions: ["explore", "continue"],
+         finalActions:["follow", "plunge down", "get back"],
+    };
