d1d91f9515fd12afc7270d65c8239d5cd1ea2aeb
LukasDekan <Lukas.dekan24@student.cart.org>
2025-04-22 19:59:27 -0700

    Updated "plungeDown" and "getBack"

--- script.js
+++ script.js
@@ -224,10 +224,123 @@
                     "name": "continueCave",
                     "text": "Upon selecting the 'Continue' option, you move through the cave and skip to the next scene."
                     }
-                ]
+                ],
                 // Text for Giant Tarantula Bossfight:
+                "giantTarantulaBossfight": [
+                {
+                "name": "bossfightIntro",
+                "text": "You continue traversing the cave, and you fly into a giant, dark room. It’s quiet at first, but suddenly you hear a loud, coarse screeching noise. You realize that there is a giant tarantula in the room, six times bigger than you and Soaren. (Same story here. You can either end the game with 'You Have Fallen' or win the fight)."
+                },
+                {
+                "name": "bossfightVictory",
+                "text": "When you deal the final blow to the giant vermin, it lets out a final, deafening screech. The tarantula faints, and falls to the ground. You have defeated it."
+                },
+                {
+                "name": "bossfightReward",
+                "text": "You then obtain a new item (Venom, can only be used once per battle, deals 10 damage) and exit the cave from the other side. You have flown through the mountain, and now you have reached the final destination."
+                }
+                ],
                 // Text for The Climax
-                // Text for THE FINALE
+                "theClimax": [
+                {
+                "name": "burningForest",
+                "text": "You approach a forest, but you breathe in a whiff of smoke through your nostrils. You then realize that the forest is burning. Soaren urges you to hurry forward, and the two of you dive down to the forest and find a dry place to land with no fire. \nYou find your father, brother, and the rest of the redwing squadron, but they are wounded by strong falcons, and some are even held captive in wooden cages. You see some of the falcons in the sky, gliding, watching over the scene, and some are bunched around the redwings, who are getting hurt. Some falcons are guarding the jailers. \nYou come closer to your father and brother. Luckily, dad is not hurt, but your brother, who’s name is Featherfoot, is limping. His foot is wounded. \n“Son! You’re here! You should have stayed home, where it was safe.”\n“It’s good to see you, Red,” says your brother (his name is Featherfoot), “but dad’s right. You should have stayed home and helped mom.”"
+                },
+                {
+                "name": "climaxTalkOptions",
+                "text": "If you choose to respond with the “talk” action, you are presented with three options. You can either greet your father and brother, tell them that they need to get out of here, or ask what happened to your brother.\nFirst option: “I’m glad to see you too, Featherfoot! And dad, I missed you so much! You look so exhausted!”\nDad: “We are indeed, son. We’ve been breathing fumes for a day and a half now!” \nHe coughs.\nFeatherfoot: “Oh dad, we need to get out of here! There’s too much smoke! I can’t see the sky in this thick fog!”\n“But the sky is watching us, and it sees us clearly! Look!” exclaims Soaren “I see falcons!”"
+                },
+                {
+                "name": "climaxSecondThirdOptions",
+                "text": "Second option: “You guys need to hurry and get out of here! The falcons are ruthless!”\nDad: “We’ve been trying to escape since day one, son. It’s not that easy. The falcons have held us captive for months now, and they lit a fire around the forest to end us!”\nSuddenly, Featherfoot looks to the sky.\n“Oh no!” he gasps, “I see falcons! They’re heading down to us!!”\nThird option: “Featherfoot, what happened to you? You’re limping!”\nFeatherfoot: “I’ve been wounded by a big falcon, Red. He came up to dad and started poking him up and mocking him, and I decided to stand up and protect him. Before I could do anything, the falcon picked me up and flew to the sky, and then he dropped me to the ground while I was ten feet above it. I was really hurt at first, but I think I’m healing now.”\nSuddenly, Featherfoot looks to the sky.\n“Oh no!” he gasps, “I see falcons! They’re heading down to us!!”"
+                },
+                {
+                "name": "climaxActAndHeal",
+                "text": "If you instead choose to “act,” you tell your brother that you will go find something to patch up his wound. You fly off, but Soaren calls you back.\n“Hold on there, little one!” he exclaims, “I’ve got a better idea.”\nHe then uses “Ointment of The Forest,” which heals your brother completely.\nYour father and brother both gasp in surprise \n“He’s healed!” exclaims the dad, “My son is healed! Oh, thank you kind sir!”\n“Don’t thank me yet, Mr. Redwing,” answers Soaren, “we must get you and your brethren out of this burning heap, and fast! But look, Little Redwing, I think we have company!”"
+                },
+                {
+                "name": "climaxCombatHeal",
+                "text": "If you choose “combat,” you can only use “Song.” When you use the move, it heals your brother completely.  \nYour father and brother both gasp in surprise.\n“Son, you’re better!”\n“No way! Red, you healed me!” says your brother “How did you do that? That was so cool bro.”\n“That was a very kind deed, little one,” says Soaren, when suddenly he exclaims “Look! I see three falcons coming our way!”"
+                },
+                {
+                "name": "climaxFalconTrio",
+                "text": "Three big falcons swoop down from the sky and land right next to you. \n“What’s dis?” says one (his name is Screecher) “Some little bird thinks he can just barge in uninvited! \n“You scoundrels!” answered Soaren, “The only ones who barged are you and your team of hooligans! Let the redwings go right this second!”\n“And who’s this old grandpa?” says the second, ignoring Soaren (his name is  Far-Cry)\n“I think it's Gandalf the Grey” says the third (his name is Thundercloud)\n“Is it Gandalf the Grey or Gandalf the White? I can’t tell!” (Far-Cry)\nThe three of them all burst out laughing.\nThe falcons are massive in size, towering over you, but you feel courage in your heart. You don’t seem to be afraid of them or frightened by them in the slightest.\nYou decide to take action."
+                },
+                {
+                "name": "climaxFightSequence",
+                "text": "If you choose to respond with the “talk” action, you stand up to the falcons.\n“Back off, bully! This is my family you’re messing with!” \n“Ah!” exclaims Far-Cry “This little fella thinks he can snap back! No problemo. I’ll just slash him!” (This is a fighting scene with the same possible death ending. In between falcons, your “Song” move resets, and you can heal on the next falcon. If you defeat Far-Cry, you move on to fight Screecher).\nFar-Cry: “Oooh! Help me...” \nHe faints. \nScreecher: “You’ll pay for this!”  (You can get the death ending here; If you defeat Screecher, you move on to fight Thundercloud).\nScreecher: “Ah! That little kid’s got some… skill…”\nHe faints.\nThundercloud: “Well, you ain’t Frodo Baggins, you’re much smaller than him! Imma send thunder over you and you’ll become a mere vapor. Think you got what it takes? Bring it on!”  \n(You can get the death ending here; If you defeat Thundercloud, you win the battle).\nThundercloud: “AAAH!!! You defeated me!”\nThundercloud falls to the ground, trembling.\n“Redwings are much more cunning than I thought. Much stronger too…” \nYou notice that he suddenly feels intimidated by you. It also occurs to you that all the other falcons were watching your fight, and they are also very scared. \nSuddenly Thundercloud begins to scream.\n“EVERYBODY! GET OUT OF HERE! THERE’S A STRONG BIRD HERE!”\nThe fog begins to clear. The cowardly falcons begin escaping and gather into a big, panicking flock, and they move westward. They are all screeching very loudly, fearing that you or Soaren might chase after them. \nThe flock slowly fades into the distance. You and the rest of the group stare at the falcons, shocked at what had just happened."
+                },
+                {
+                "name": "climaxVictoryEscape",
+                "text": "And then… the crowd goes wild. The captive redwings are now free! You saved them all! You are a HERO!\nEveryone flies to you and Soaren, lifting you up from the ground and tossing you into the air. The crowd chants: “Hoorah for Little Red! Hoorah for Little Red!”\n“I’m very proud of you, son,” says your father, “you saved your brethren, and we are grateful for your bravery and determination.”\n“Yeah, Red,” says Featherfoot, “That was sick! I didn’t know you were so good at fighting!”\n“I knew you could do it, little redwing,” says Soaren, “after everything we’ve been through together, this was one of the greatest battles we’ve ever fought. Well done!”\nAs the crowd thanks you, you are reminded of the fact that the tall trees surrounding you are still burning. They are like tall towers, imposing themselves over you and your friends.\nAnd then, realization hits you… the trees are collapsing. \n“THE TREES!” shouts Featherfoot “THEY ARE FALLING ON US!”\nThe flock starts to panic, but in the same instant, everyone goes airborne.\n“Hurry, son!” your father shouts. \nYou follow suit.\nEvery tree in the forest is now crashing, one by one. However, your flock is led by Soaren, who is a master at maneuvering. He directs everyone to follow his movements:\n“This way!” he shouts, as you approach the exit.\nYou are following close behind everybody, and your brother and father are right in front of you. Suddenly, a colossal conifer begins to topple over everyone.\n“HURRY!” shouts the owl, “WE CAN STILL MAKE IT!”\nEverybody gets away, to the other side, but you are behind everybody, trying the best you can to get close to the group, but you are too slow. \nYOU NOW SEE THE TREE FALLING ON YOU. You can still make it across, but you also see a hole in the ground beneath you. \nYOU HAVE SIX SECONDS TO MAKE YOUR NEXT MOVE.\nYou are now presented with three options: you can either get back and fly in the opposite direction, away from the flock; you can plunge down into the hole beneath you, or you can follow the rest and cross to the other side."
+                }
+                ],
+                // Text for THE FINALE:
+                // Upon choosing the "getBack" option from finalActions, the user will see this text:
+                "getBack": [
+                    {
+                        "name": "evadeTree",
+                        "text": "If you choose the get back option, you evade the falling tree just in time. It crashes with a loud thump, and a branch from the tree breaks off and hurts you. You faint.\nSuddenly you hear a voice.\n“Wake up.”"
+                    },
+                    {
+                        "name": "wakeInFire",
+                        "text": "You open your eyes, and your vision is blurred. You can make out, however, that you are surrounded by flames. You are coughing.\n“The smoke is really hitting you, huh?” says the voice.\nYour vision clears and you try to get a closer look at the speaker.\nIt is a falcon.\nWhat should you do?"
+                    },
+                    {
+                        "name": "combatBlocked",
+                        "text": "If you choose combat, the dialogue box tells you that you cannot fight because your strength is at zero. The game takes you back to “actions.”"
+                    },
+                    {
+                        "name": "eatBlocked",
+                        "text": "If you choose to eat, the dialogue box tells you that you are too weak to make any movements (this will still be the case if you choose the “seeds” item from your inventory)"
+                    },
+                    {
+                        "name": "actFails",
+                        "text": "If you choose to act, you try to get up, but you tremble and fall back to the ground.\n“Don’t try that,” says the falcon, “you are too weak. But you will soon be strengthened. Come with me.”"
+                    },
+                    {
+                        "name": "talkPrompt",
+                        "text": "If you choose to talk, you are presented with two options."
+                    },
+                    {
+                        "name": "talkOption1",
+                        "text": "“Why aren’t you trying to kill me?”" 
+                    },
+                    {
+                        "name": "outcome1",
+                        "text": "“I do not want you to die,” the falcon says, “because you are my prisoner. Come with me.”"
+                    },
+                    {
+                        "name": "talkOption2",
+                        "text": "“Get Away!”" 
+                    },
+                    {
+                        "name": "outcome2",
+                        "text": "“Why should I?” says the falcon, “I am only here to help you. Come with me.”"
+                    },
+                    {
+                        "name": "faintAgain",
+                        "text": "You shut your eyes again, and your strength is failing. You faint once again, but you feel yourself being lifted from the ground.\nAnd then you feel the air. It is fresh air. But you feel a certain chill in your feathers. A chill… of winter…"
+                    }
+                    ],
+                // Upon choosing the "plungeDown" option from finalActions, the user will see this text:
+                "plungeDown": [
+                {
+                    "name": "plungeIntoHole",
+                    "text": "If you choose the plunge down option, you plunge down into the hole beneath you. The tree falls with a loud thump, covering the hole. You are safe inside, but there is no light. You call out for help, but no one hears you. After crying out for about an hour, you decide to explore the hole. It turns out to be a cave system."
+                },
+                {
+                    "name": "meetGlydeAndClancy",
+                    "text": "After exploring for a while, you find an old owl and a crow.\n“Greetings, little one,” says the owl, “I am Glyde, brother of Soaren. And this crow sitting in front of me is Clancy, my companion. We have been waiting for someone important to arrive.”"
+                },
+                {
+                    "name": "stayInCave",
+                    "text": "“Yes,” says the crow, “we’ve been sitting here, in this cave, for two months now, with no sunlight, and we are quite lonesome. You look like a friendly fellow. Would you care to stay with us until our awaited traveler returns?”\nYou decide to keep them company and stay with them until the traveler comes back."
+                }
+                ],
+                
+                // Upon choosing the "follow" option from finalActions, the user will see this text:
+
             },
 
             
@@ -275,7 +388,7 @@
         document.addEventListener('DOMContentLoaded', function() {
         document.getElementById('nextButton').addEventListener('click', function() {
                 // Optional: reset each time button is clicked
-                story.i = 0;
+                // story.i = 0;
                 document.getElementById('text').innerHTML = '';
                 story.writeText();
             });
